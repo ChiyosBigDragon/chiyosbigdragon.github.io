eb6fed8e63e6b58e785a1b223f123c35b5748d0c
ChiyosBigDragon <chiyotairyuhidemasa@gmail.com>
2019-03-25 23:09:05 +0900

    fixed

---
+++ js/srcReader.js
@@ -0,0 +1,20 @@
+// <pre><code>にsrc属性を追加
+// <head>でjQueryを呼ぶことに注意
+
+$( function() {
+    $('pre code').each( function(index, codeTag) {
+        var obj = $(this);
+        // srcがあるか
+        if(typeof obj.attr('src') !== "undefined") {
+            // URL取得
+            var url = obj.attr('src');
+            // URLからコード取得
+            $.get(url, function(code) {
+                // 書き換え
+                obj.text(code);
+                // ハイライト
+                hljs.highlightBlock(codeTag);
+            });
+        }
+    });
+});
