8b9cb53353355fec2d79ac1d30599302e8bd584e
ChiyosBigDragon <chiyotairyuhidemasa@gmail.com>
2019-08-15 14:55:05 +0900

    srm757d1e

--- js/srcReader.js
+++ js/srcReader.js
@@ -1,21 +1,38 @@
 // <pre><code>にsrc属性を追加
+// <pre><code>に折りたたみ機能を追加
 // <head>でjQueryを呼ぶことに注意
 
 // DOMを読み込むまで動作しない
-$( function() {
-    $('pre code').each( function(index, codeTag) {
-        var obj = $(this);
-        // srcがあるか
-        if(typeof obj.attr('src') !== "undefined") {
-            // URL取得
-            var url = obj.attr('src');
-            // URLからコード取得
-            $.get(url, function(code) {
-                // 書き換え
-                obj.text(code);
-                // ハイライト
-                hljs.highlightBlock(codeTag);
-            });
-        }
+$(function() {
+    $('.SyntaxHighlight').each(function(index, codeTag) {
+		var textArea = $('div', this);
+		var codeArea = $('pre code', this);
+		// src追加
+		codeArea.each( function(index, codeTag) {
+			var obj = $(this);
+			// srcがあるか
+			if(typeof obj.attr('src') !== "undefined") {
+				// URL取得
+				var url = obj.attr('src');
+				// URLからコード取得
+				$.get(url, function(code) {
+					// 書き換え
+					obj.text(code);
+					// ハイライト
+					hljs.highlightBlock(codeTag);
+				});
+			}
+		});
+		// 折りたたみ
+		textArea.on('click', function() {
+			var txt = $(this).text();
+			if(txt == "[展開する]") {
+				$(this).text("[折りたたむ]");
+				codeArea.css('max-height', 'none');
+			} else {
+				$(this).text("[展開する]");
+				codeArea.css('max-height', '300px');
+			}
+	   	});
     });
 });
